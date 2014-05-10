33db00bb8d62f3581acefe4c55cfd10077711e69
reddy.anddev@gmail.com <Shekhar.Nareddy@IAD1-LDL-20092.synapse.com>
2014-05-10 10:09:21 -0500

    Added Navigation Up support for Information Activity

--- src/com/hce/sample/InfoActivity.java
+++ src/com/hce/sample/InfoActivity.java
@@ -1,13 +1,28 @@
 package com.hce.sample;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 
+/**
+ * 
+ * @author Shekhar.Nareddy
+ * Information about how to use this Demo Application.
+ *
+ */
 public class InfoActivity extends ActionBarActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_info);
+		ActionBar actionBar = getSupportActionBar();
+		actionBar.setDisplayHomeAsUpEnabled(true);
+	}
+	
+	@Override
+	public boolean onSupportNavigateUp() {
+		finish();
+		return super.onSupportNavigateUp();
 	}
 
 }
