fa46143eda2fa4bf95703bdbfe18d9dc091d7b5a
reddy.anddev@gmail.com <Shekhar.Nareddy@IAD1-LDL-20092.synapse.com>
2014-05-10 10:03:34 -0500

    Added Information Activity

--- src/com/hce/sample/HCEHomeActivity.java
+++ src/com/hce/sample/HCEHomeActivity.java
@@ -162,7 +162,9 @@ public class HCEHomeActivity extends ActionBarActivity {
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
+		if (id == R.id.action_info) {
+			final Intent infoIntent = new Intent(getApplicationContext(), InfoActivity.class);
+			startActivity(infoIntent);
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
